bd5f5a2b76de3841ec08f217ed293e8afb32aeab
clbonner <clawrence.bonner@googlemail.com>
2017-07-21 08:31:35 +0000

    Removed login icon from menu.

--- views/header.php
+++ views/header.php
@@ -28,9 +28,7 @@
             <a class="btn-menu left" href="<?= SITEURL ?>/search.php"><i class="fa fa-search menu-icon"></i></a>
         </div>
         <div class="right">
-            <?php if(!isset($_SESSION["userid"])): ?>
-                <a class="btn-menu left" href="<?= SITEURL ?>/admin/login.php"><i class="fa fa-sign-in menu-icon"></i></a>
-            <?php else: ?>
+            <?php if(isset($_SESSION["userid"])): ?>
                 <a class="btn-menu left" href="<?= SITEURL ?>/admin/prefs.php"><i class="fa fa-cog menu-icon"></i></a>
                 <a class="btn-menu left" href="<?= SITEURL ?>/admin/logout.php"><i class="fa fa-sign-out menu-icon"></i></a>
             <?php endif ?>

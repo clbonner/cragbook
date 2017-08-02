49caeab1e1272935bcf0765cefffc2d93cb00676
clbonner <clawrence.bonner@googlemail.com>
2017-08-02 10:24:15 +0000

    Added Help button to site menu.

---
+++ views/help.php
@@ -0,0 +1,23 @@
+<div class="content panel">
+    <div class="title">Help</div>
+    <br>
+    <div>
+        <p>Using Cragbook is very simple. You can look for information in one of three ways using the site menu above.</p>
+        
+        <div>
+            <p><span class="btn btn-border">Areas</span> Shows you a list of areas, wherein lies all the crags for that area.</p>
+            <p><span class="btn btn-border">Crags</span> Shows you all crags regardless of areas.</p>
+            <p><i class="fa fa-search btn btn-border"></i> Allows you to find routes by using one or more search criteria.</p>
+        </div>
+
+        <br>
+        <p>When viewing areas or crags, each page has some buttons that do the following. </p>
+        
+        <div id="viewpicker">
+            <p><i class="fa fa-list btn btn-border"></i> Displays a list of areas/crags.</p>
+            <p><i class="fa fa-info btn btn-border"></i> Shows crag information on crag pages.</p>
+            <p><i class="fa fa-map-o btn btn-border"></i> Shows a map of areas/crags.</p>
+            <p><i class="fa fa-print btn btn-border"></i> Prints a list of currently selected routes and area/crag information.</p>
+        </div>
+    </div>
+</div>

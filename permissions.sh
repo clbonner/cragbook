cdd16dd5ac8ec33fea441312c56548a94902c776
clbonner <clawrence.bonner@gmail.com>
2020-05-02 19:58:12 +0100

    New permissions script to set correct permissions on files and directories for production use.

---
+++ permissions.sh
@@ -0,0 +1,18 @@
+# This file is part of Cragbook https://github.com/clbonner/cragbook
+# and is licensesed under the GNU General Public License version 3.
+# Copyright 2017 Christopher L Bonner
+#
+# permissions.sh
+# Sets all the correct permissions on files and folders.
+
+chmod 755 ./admin
+chmod 755 ./css
+chmod 755 ./css/font-awesome
+chmod 755 ./css/font-awesome/css
+chmod 755 ./css/font-awesome/fonts
+chmod 755 ./css/font-awesome/less
+chmod 755 ./css/font-awesome/scss
+chmod 755 ./include
+chmod 755 ./js
+chmod 755 ./view
+chmod -R ./*.php 644

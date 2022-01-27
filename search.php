346eca85a9523a34b90fe9cb1446985da9e264ee
C L Bonner <clawrence.bonner@gmail.com>
2022-01-27 09:34:15 +0000

    Update search.php
    Removed redundant code.

--- search.php
+++ search.php
@@ -10,72 +10,7 @@
  
 require_once(__DIR__ ."/include/config.php");
 
-// search database
-if ($_GET["search"] != NULL)
-{
-    $db = db_connect();
-    
-    $search = sec_check($_GET["search"]);
-    
-    // search areas
-    $sql = "SELECT * FROM areas WHERE LCASE(name) LIKE LCASE(\"%" .$search ."%\");";
-    
-    if (!$result = $db->query($sql))
-        error("Error in search.php: " .$db->error);
-    elseif ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) 
-            $areas[$row["areaid"]] = $row["name"];
-    }
-    else
-        $areas = 0;
-    
-    // search crags
-    $sql = "SELECT * FROM crags WHERE LCASE(name) LIKE LCASE(\"%" .$search ."%\");";
-    
-    if (!$result = $db->query($sql))
-        error("Error in search.php: " .$db->error);
-    elseif ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) 
-            $crags[$row["cragid"]] = $row["name"];
-    }
-    else
-        $crags = 0;
-    
-    // search routes
-    $sql = "SELECT * FROM routes WHERE (LCASE(name) LIKE LCASE(\"%" .$search ."%\")) OR (grade LIKE \"%" .$search ."%\") ORDER BY name ASC;";
-    
-    if (!$result = $db->query($sql))
-        error("Error in search.php: " .$db->error);
-    elseif ($result->num_rows > 0) {
-        $routes = [];
-        while($row = $result->fetch_assoc()) 
-            array_push($routes, $row);
-    }
-    else
-        $routes = 0;
-    
-    // get crag list for routes found
-    $sql = "SELECT cragid, name FROM crags";
-    
-    if (!$result = $db->query($sql))
-        error("Error in search.php: " .$db->error);
-    elseif ($result->num_rows > 0) {
-        $craglist = [];
-        while($row = $result->fetch_assoc())
-            array_push($craglist, $row);
-    }
-    else
-        $craglist = 0;
-    
-    view("search_results.php", ["areas" => $areas, "crags" => $crags, "routes" => $routes, "craglist" => $craglist]);
-    
-    $db->close();
-}
-
 // show search form
-else
-{
-    view("search.php", []);
-}
+view("search.php", []);
 
 ?>
